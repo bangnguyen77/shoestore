fc06c16f1c4f0e45faad9ee38db8b77a3a2de387
Bang Nguyen <bangnv77@gmail.com>
2016-07-22 09:16:26 -0700

    modify app.rb and add views pages: index, layout and store_form

--- app.rb
+++ app.rb
@@ -3,3 +3,28 @@ Bundler.require(:default)
 require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
+
+get("/") do
+  erb(:index)
+end
+
+get("/stores/new") do
+  @brands = Brand.all()
+  erb(:store_form)
+end
+
+post("/brands") do
+  name = params.fetch("name")
+  Brand.create({:name => name})
+  redirect back
+end
+
+post("/stores") do
+  name = params.fetch("name")
+  brands = params.fetch("brand_ids")
+  @brands = []
+  brand_ids.each() do |brand_id|
+    brand = Brand.find(brand_id)
+    @brands.push(brand)
+  end
+end
