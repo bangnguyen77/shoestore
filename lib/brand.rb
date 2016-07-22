4ff3d9610d49547177a237412672121df2a89158
Bang Nguyen <bangnv77@gmail.com>
2016-07-22 08:31:18 -0700

    initialize database and Classes

---
+++ lib/brand.rb
@@ -0,0 +1,12 @@
+class Brand < ActiveRecord::Base
+  has_and_belongs_to_many(:stores)
+
+#   validates(:name, {:presence => true, :length => {:maximum => 50}})
+#
+#   before_save(:capitalize_name)
+#
+# private
+#   define_method(:capitalize_name) do
+#     self.name=(name().capitalize())
+#   end
+end
